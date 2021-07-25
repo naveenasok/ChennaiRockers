3a9b3d68e14e315b2bf8af2f5c0f9e731d1be2d8
naveenasok <naveenasokan46@gmail.com>
2021-07-25 22:23:49 +0530

    Task is completed

---
+++ src/test/java/org/pack/Sample.java
@@ -0,0 +1,15 @@
+package org.pack;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tree");
+		System.out.println("Plant");
+		System.out.println("seed");
+			
+		
+	}
+	
+	
+}
